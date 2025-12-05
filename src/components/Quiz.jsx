26a6a62eadbb80e54c42283d6921b8b56b6b092b
Renzy Antonio <renzy.antonio0112@gmail.com>
2025-12-05 21:28:25 +0800

    add API security, displaying of answer choices, and changed naming conventions

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { decode } from "html-entities";
 import mockUpData from "../mockup.json";
 import Loading from "./Loading";
 
@@ -9,9 +10,7 @@ function Quiz() {
   useEffect(() => {
     const getResponse = async () => {
       try {
-        const response = await fetch(
-          "https://opentdb.com/api.php?amount=5&type=multiple"
-        );
+        const response = await fetch(import.meta.env.VITE_APP_API);
         if (!response.ok) {
           throw new Error("There was a problem with the server.");
         }
@@ -28,27 +27,32 @@ function Quiz() {
   }, []);
 
   const displayResult = result.map((data, index) => {
+    const displayQuestion = decode(data.question);
+    const answerArray = [...data.incorrect_answers, data.correct_answer].sort(
+      () => Math.random() - 0.5
+    );
+
+    const displayChoice = answerArray.map((choice, index) => {
+      return (
+        <button
+          className="border-1 border-lavender-button-start rounded-lg py-1 px-2 text-xs font-inter font-medium text-lavender-font hover:cursor-pointer active:scale-90 transform duration-200 ease-in-out"
+          key={index}
+        >
+          {decode(choice)}
+        </button>
+      );
+    });
+
     return (
       <div
         className="w-full flex flex-col justify-start items-start space-y-3"
         key={index}
       >
         <h3 className="text-lavender-font text-lg font-karla font-bold break-all">
-          {data.question}
+          {displayQuestion}
         </h3>
         <div className="max-w-full flex flex-wrap justify-start items-center gap-3">
-          <button className="border-1 border-lavender-button-start rounded-lg py-1 px-2 text-xs font-inter font-medium text-lavender-font hover:cursor-pointer active:scale-90 transform duration-200 ease-in-out">
-            {data.correct_answer}
-          </button>
-          <button className="border-1 border-lavender-button-start rounded-lg py-1 px-2 text-xs font-inter font-medium text-lavender-font hover:cursor-pointer active:scale-90 transform duration-200 ease-in-out">
-            Transformers
-          </button>
-          <button className="border-1 border-lavender-button-start rounded-lg py-1 px-2 text-xs font-inter font-medium text-lavender-font hover:cursor-pointer active:scale-90 transform duration-200 ease-in-out">
-            Care Bears
-          </button>
-          <button className="border-1 border-lavender-button-start rounded-lg py-1 px-2 text-xs font-inter font-medium text-lavender-font hover:cursor-pointer active:scale-90 transform duration-200 ease-in-out">
-            Rubik's Cube
-          </button>
+          {displayChoice}
         </div>
         <hr className="w-full border-b-2 border-lavender-button mt-3.5" />
       </div>
