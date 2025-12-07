ac2ff168d7565bc44511484f7a14c738ee2545ab
Renzy Antonio <renzy.antonio0112@gmail.com>
2025-12-07 18:30:17 +0800

    created Toast component

---
+++ src/components/Toast.jsx
@@ -0,0 +1,13 @@
+function Toast() {
+  return (
+    <div className="toast toast-top toast-center">
+      <div className="alert alert-error">
+        <span className="font-karla font-medium text-white">
+          Please answer all of the questions.
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Toast;
